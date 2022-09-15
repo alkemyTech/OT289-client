5ea08c0809207e0bf4f8b147bbf5126e9e296d16
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-15 12:26:40 -0300

    finished the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header/Header'
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <>
+      <Header/>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
@@ -52,6 +55,7 @@ function App() {
         </span>
       </header>
     </div>
+    </>
   );
 }
 
