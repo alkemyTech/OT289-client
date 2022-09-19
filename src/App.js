94e3835d75e4208cc969498d29715046241df503
danielDeVita <danielitodevita@gmail.com>
2022-09-19 16:27:05 -0300

    edit form for home, first attempt, no styles and hardcoded data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/Home';
 import News from './components/News';
 import About from './components/About';
+import Slider from './components/editForm';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
         <Route path="/about" element={<About />} />
+        <Route path="/editForm" element={<Slider />} />
       </Routes>
     </div>
   );
