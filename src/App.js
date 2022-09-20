a56c755c5554e7d912cc9ebb652f9b82bfb8e82c
danielDeVita <danielitodevita@gmail.com>
2022-09-20 17:43:58 -0300

    2nd attempt at slider with text area as form to edit home content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
-
-import Home from './components/Home';
-import News from './components/News';
-import About from './components/About';
-import Slider from './components/editForm';
-import { Routes, Route } from 'react-router-dom';
+import ImageSlider from './components/ImageSlider';
 
 function App() {
+  const slides = [
+    { url: "https://images.unsplash.com/photo-1661983228910-eb340412e0c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80", title: "Guy Computer" },
+    { url: "https://images.unsplash.com/photo-1662047919286-19175909fbcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80", title: "Mountain" },
+    { url: "https://images.unsplash.com/photo-1662100112119-09f64f7c288e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80", title: "Freeway" }
+  ];
+ 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/editForm" element={<Slider />} />
-      </Routes>
+        <ImageSlider slides={slides} />
     </div>
   );
 }
