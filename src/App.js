34ba3b9d65daaedb39397cea5b7fb34626770421
danielDeVita <danielitodevita@gmail.com>
2022-09-21 15:48:48 -0300

    form to edit home view, with required text area and minlength = 20, 3 file inputs and 3 text inputs (each with label)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import ImageSlider from './components/ImageSlider';
+import EditForm from './components/EditForm';
 
 function App() {
-  const slides = [
-    { url: "https://images.unsplash.com/photo-1661983228910-eb340412e0c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80", title: "Text for the 1st image" },
-    { url: "https://images.unsplash.com/photo-1662047919286-19175909fbcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80", title: "Text for the 2nd image" },
-    { url: "https://images.unsplash.com/photo-1662100112119-09f64f7c288e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80", title: "Text for the 3rd image" }
-  ];
 
   return (
     <div className="App">
-      <ImageSlider slides={slides} />
+      <EditForm />
     </div>
   );
 }
