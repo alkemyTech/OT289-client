56e867fd90037c6ee741c21834270943be37f57f
Gabriel <ahrensgabriel09@gmail.com>
2022-09-21 10:39:30 -0300

    feat/MainScreen complete HTML structure

--- src/features/mainScreen/Main.jsx
+++ src/features/mainScreen/Main.jsx
@@ -3,9 +3,10 @@ import Slider from "../slider/Slider";
 import NewsletterCard from "./NewsletterCard";
 import "./Main.css";
 
-const defaultText =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At quas explicabo, sit animi deleniti dicta? Deleniti veritatis est temporibus voluptates nesciunt hic, deserunt facere delectus voluptatum, consequatur, quos maiores quam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi nisi, nemo asperiores ad.";
+const defaultText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At quas explicabo, sit animi deleniti dicta? Deleniti veritatis est temporibus voluptates nesciunt hic, deserunt facere delectus voluptatum, consequatur, quos maiores quam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi nisi, nemo asperiores ad.";
+
 const defaultTitle = "HOLA BIENVENIDX";
+
 const defaultImg = [
   {
     imageUrl: 'images/campaign-recent-01.jpg',
@@ -20,9 +21,27 @@ const defaultImg = [
     text: "HEROO",
   },
 ];
-const news = [{}];
 
-const Main = ({ text = defaultText, title = defaultTitle, img = defaultImg }) => {
+const defaultNews = [
+  {
+    img: "images/campaign-big-item.jpg",
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci accusantium animi quasi nulla obcaecati vero id, quo voluptates necessitatibus.'
+  },
+  {
+    img: "images/campaign-tabs.jpg",
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci accusantium animi quasi nulla obcaecati vero id, quo voluptates necessitatibus.'
+  },
+  {
+    img: "images/featured-places-01.jpg",
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci accusantium animi quasi nulla obcaecati vero id, quo voluptates necessitatibus.'
+  },
+  {
+    img: "images/featured-places-02.jpg",
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci accusantium animi quasi nulla obcaecati vero id, quo voluptates necessitatibus.'
+  }
+];
+
+const Main = ({ text = defaultText, title = defaultTitle, img = defaultImg, newsletter= defaultNews }) => {
   return (
     <div className="body-container">
       <div className="hero">
@@ -30,7 +49,7 @@ const Main = ({ text = defaultText, title = defaultTitle, img = defaultImg }) =>
           <h2 className="title">{title}</h2>
           <p className="welcome-text">{text}</p>
           <div>
-            <button className="button">Contactanos</button>
+            <button className="main-button">Contactanos</button>
           </div>
         </div>
         <div className="slider-div">
@@ -38,10 +57,9 @@ const Main = ({ text = defaultText, title = defaultTitle, img = defaultImg }) =>
         </div>
       </div>
       <div className="news">
-        <NewsletterCard />
-        <NewsletterCard />
-        <NewsletterCard />
-        <NewsletterCard />
+        {
+          newsletter.map(n => (<NewsletterCard img={n.img} text={n.text}/>))
+        }
       </div>
     </div>
   );
