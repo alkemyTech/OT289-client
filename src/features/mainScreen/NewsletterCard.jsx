1557b07e882ea2e410cef3545af82dacde21258f
Gabriel <ahrensgabriel09@gmail.com>
2022-09-21 10:41:56 -0300

    feat/NewsletterCard complete HTML structure

--- src/features/mainScreen/NewsletterCard.jsx
+++ src/features/mainScreen/NewsletterCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
+import './NewsletterCard.css'
 
-const NewsletterCard = () => {
+const NewsletterCard = ({ img, text }) => {
   return (
     <div className='card-container'>
         <div className='img-container'>
-            <img  src="images/campaign-big-item.jpg" alt="Not found" width="150" height="150"/>
+            <img className='new-img' src={img} alt="Not found" width="150" height="150"/>
         </div>
         <div className='info-container'>
             <div className='preview-container'>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci accusantium animi quasi nulla obcaecati vero id, quo voluptates necessitatibus. Mollitia eum necessitatibus harum laborum repudiandae doloremque veniam commodi. Obcaecati, ipsa?</p>
+                <p className='new-text'>{text}</p>
             </div>
-            <div className='button-card'>
-                <button>View More</button>
+            <div className='new-button-card'>
+                <button className='new-button'>Ver Novedad</button>
             </div>
         </div>
-
     </div>
   )
 }
