edbba9d2e76a30e1e7c6ef0895c373c8f301c303
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-24 22:17:07 -0300

    reverted app to main status

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import React from 'react';
 import './App.css';
-//import EditForm from './features/editForm/EditForm';
+import EditForm from './components/EditForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ScreenContact from './features/screencontac/ScreenContac';
-import { Routes, Route } from 'react-router-dom';
-import {useSelector} from 'react-redux';
-import {useDispatch} from 'react-redux';
-import {login, logout, refresh} from './features/user/userSlice'
 
+import ScreenContact from './features/screencontac/ScreenContac';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYW1lIjoiZ3Vlc3QiLCJlbWFpbCI6Imd1ZXN0QGdtYWlsLmNvbSJ9.-YkrY52fBf0X7SksxYmneGp2jzY8kyIT6euGCVWcdNo'
-  const userState = useSelector(state => state.user)
-  const dispatch = useDispatch()
-
-  dispatch(refresh())
-  
-  const handleLogin = () => {
-    localStorage.setItem('token', token)
-    dispatch(login(token))
-  }
-
-  const handleLogout = () => {
-    localStorage.clear('token')
-    dispatch(logout())
-  }
-
-  return (  
+  return (
     <div className="App">
-      <h1>el id del usuario actual es</h1>
-      <h3>{userState.id}</h3>
-      <h1>y el email del usuario actual es</h1>
-      <h3>{userState.email}</h3>
-      <h1>Y su token es</h1>
-      <h3>{userState.token}</h3>
-      <button onClick={() => handleLogin()}>boton login</button>
-      <button onClick={() => handleLogout()}>boton logout</button>
-      
+      <EditForm />
       <Routes>
         <Route path="/contac" element={<ScreenContact />} />
       </Routes>
-    </div>  
+    </div>
   );
 }
 
