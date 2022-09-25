4593d41c21c0075b1b7ced963ae0d8582261aa82
gervy gonzalez <gervygonzalez@gmail.com>
2022-09-25 18:17:01 -0300

    fature/OT289-48 screen news render card and letter C in alert

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import EditForm from './components/EditForm';
+import EditForm from './features/editForm/EditForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ScreenContact from './features/screencontac/ScreenContac';
-
+import ScreenNews from './features/screenNews/screenNews';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <EditForm />
+      {  <EditForm /> }
       <Routes>
         <Route path="/contac" element={<ScreenContact />} />
+        <Route path="/news" element={<ScreenNews/>} />
       </Routes>
     </div>
   );
