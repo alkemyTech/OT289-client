10cb401130a6f900f1d0e019550056dd7d7d5313
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-26 20:11:50 -0300

    going to give app component original state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-//import EditForm from './features/editForm/EditForm';
+import EditForm from './features/editForm/EditForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import jwtDecode from 'jwt-decode'
 
@@ -9,18 +9,9 @@ import ScreenContact from './features/screencontac/ScreenContac';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImZhZGFzZGFkYXNkSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsInJvbGVJZCI6Mn0.btTzRLfmCJycJ8tgi4kzs965XHe6trFEtwmefnguLPQ'
-  localStorage.setItem('token', token)
-  const lsToken = localStorage.getItem('token')
-  console.log()
-  const decodedToken = jwtDecode(lsToken) // Si cambias el signature del token igualmente obtenes los datos del payload!, para preguntar el lunes
-
-  console.log(decodedToken)
-
   return (
     <div className="App">
-      
+      <EditForm />
       <Routes>
         <Route path="/contac" element={<ScreenContact />} />
       </Routes>
