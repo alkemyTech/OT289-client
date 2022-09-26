04d0618ccc81cd38e0b43598309a72d9da215eb6
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-26 20:12:26 -0300

    app component to original state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import EditForm from './features/editForm/EditForm';
+import EditForm from './components/EditForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import jwtDecode from 'jwt-decode'
 
 import ScreenContact from './features/screencontac/ScreenContac';
 
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
       <EditForm />
