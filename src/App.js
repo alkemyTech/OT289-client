8fef8d251e80aa9a09b7f8896e95e26fdcc8509f
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-10-02 21:45:20 -0300

    finished the backoffice, have to had the controller to put the news, so for now the news are in an array

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BackOffice from './features/backOffice/BackOffice';
+import NewsPanel from './features/backOffice/partials/NewsPanel'
 
 import ScreenContact from './features/screencontac/ScreenContac';
 
 import { Routes, Route } from 'react-router-dom';
-import NewsPanel from './features/backOffice/partials/NewsPanel';
+
 
 function App() {
 
   return (
     <div className="App">
-      <NewsPanel />
       <Routes>
         <Route path="/contac" element={<ScreenContact />} />
         <Route path="/backOffice/*" element={<BackOffice />} />
+        <Route exact path='/backoffice/news' element={<NewsPanel />} />
       </Routes>
     </div>
     
