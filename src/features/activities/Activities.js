b5543cf84f8f3d3f8a3b99a9a56dc46501e27f52
danielDeVita <danielitodevita@gmail.com>
2022-10-03 13:59:59 -0300

    added more fields in activities component table

--- src/features/activities/Activities.js
+++ src/features/activities/Activities.js
@@ -3,7 +3,34 @@ import './activities.css';
 import { useState, useEffect } from 'react';
 
 /* DUMMY EXAMPLE ACTIVITIES */
-let activities = ["activity1", "activity2", "activity3", "activity4"];
+// let activities = ["activity1", "activity2", "activity3", "activity4"];
+
+let activities = [
+    {
+        id: 1,
+        name: "activity 1",
+        image: "https://images.pexels.com/photos/8813498/pexels-photo-8813498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        id: 2,
+        name: "activity 2",
+        image: "https://images.pexels.com/photos/8471859/pexels-photo-8471859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    },
+    {
+        id: 3,
+        name: "activity 3",
+        image: "https://images.pexels.com/photos/296301/pexels-photo-296301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+    {
+        id: 4,
+        name: "activity 4",
+        image: "https://images.pexels.com/photos/61129/pexels-photo-61129.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+]
 
 /* FETCH AND HOOKS TO BE USED ONCE ACTIVITIES ENDPOINT IS COMPLETED */
 
@@ -26,14 +53,18 @@ const Activities = () => {
 
         <table>
             <tr>
-                <th>Activity</th>
-                <th>Edit Activity</th>
-                <th>Delete Activity</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Descripción</th>
+                <th>Editar</th>
+                <th>Borrar</th>
             </tr>
 
             {activities.map((activity, index) => (
-                <tr key={index}>
-                    <td>{activity}</td>
+                <tr key={activity.id}>
+                    <td>{activity.name}</td>
+                    <td><img width="200px" src={activity.image} alt="activity image" /></td>
+                    <td><p>{activity.content}</p></td>
                     <td>Edit</td>
                     <td>Delete</td>
                 </tr>
