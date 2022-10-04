75c6ba1cd4baabf9f59786b29820666aea7f5150
gervy gonzalez <gervygonzalez@gmail.com>
2022-10-03 22:42:22 -0300

    feature/OT289-48

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      {  <EditForm /> }
+     {<EditForm />}
       <Routes>
         <Route path="/contac" element={<ScreenContact />} />
         <Route path="/news" element={<ScreenNews/>} />
