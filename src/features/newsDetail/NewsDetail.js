69859bc642b91acc7bacd2b9d1d01815c62285fc
Facundo Azcue <fazcue@gmail.com>
2022-10-07 19:43:27 -0300

    Add customFetch, goBack button. Reorder components. Add some extra styles

--- src/features/newsDetail/NewsDetail.js
+++ src/features/newsDetail/NewsDetail.js
@@ -1,79 +1,88 @@
-import React, {useState,useEffect} from "react"
-import "./NewsDetail.css"
-import publicService from "../../services/publicService"
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import './NewsDetail.css'
+import { customFetch } from '../../services/fetch'
+import { useParams } from 'react-router-dom'
 
-const NewsDetail = ({defaultNews}) => {
+const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL
 
-    const [news, setNews] = useState(defaultNews)
+const NewsDetail = () => {
+    const [news, setNews] = useState(null)
 
-    const getNews = async (values) => {
-        let response = await publicService.newsDetail(1)
-        setNews(response.data)
+    const params = useParams()
+    const { id } = params
+
+    const getNews = async () => {
+        try {
+            const url = `${SERVER_BASE_URL}/news/${id}`
+            const properties = {
+                method: 'get'
+            }
+
+            const res = await customFetch(url, properties)
+            setNews(res.data)
+        } catch (error) {
+            console.log('Error getting news: ', error)
+        }
+    }
+
+    const Header = ({ title, image, updatedAt }) => {
+        const date = new Date(updatedAt)
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    
+        return (
+            <header className="newsHeader d-flex align-items-center" style={{backgroundImage: 'url(' + image + ')'}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-10 mx-auto">
+                            <div className="newsHeadingContainer">
+                                <h1 className="newsTitle">{title}</h1>
+                                <span className="newsDate">{date.toLocaleDateString('es-AR', options)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+
+    const Content = ({ content }) => {
+        return (
+            <div className="newsBody">
+                <div className="row">
+                    <div className="col-md-10 col-lg-8 mx-auto newsContent">
+                        {content}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const GoBackButton = () => {
+        return (
+            <Button href='/novedades' variant="outline-primary" className='button'>Ver más novedades</Button>
+        )
     }
 
     useEffect(()=>{
         getNews()
     },[])
 
-    return (
-        <>
-            <Header title={news.name} datetime={news.createdAt} image={news.image}/>
-            <Content content={news.content}/>
-        </>
-    )
-}
-
-
-NewsDetail.defaultProps = {defaultNews:
-    {title:'',content:'',createdAt:new Date().toISOString()}
-}
-
-
-const Header = ({title, datetime, image}) => {
-
-    const date = new Date(datetime);
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-
-    let imageUrl = "http://localhost:3001/images/news/"
-
-    return (
-        
-        <header className="newsHeader d-flex align-items-center" style={{backgroundImage: 'url(' + imageUrl+ image + ')'}}>
-        
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8 col-md-10 mx-auto">
-                        <div className="newsHeadingContainer">
-                            <h1 className="newsTitle">{title}</h1>
-                            <span className="newsDate">{date.toLocaleDateString('es-AR', options)}</span>
-                        </div>
-                    </div>
-                </div>
+    if (!news) {
+        return (
+            <div className='mainContainer'>
+                <h1>Cargando novedad</h1>
             </div>
-        </header>
-    
-    )
-}
-
-
-
-const Content = ({content}) => {
+        )
+    }
 
     return (
-        
-        <div className="newsBody">
-            <div className="row">
-                <div className="col-md-10 col-lg-8 mx-auto">
-                    <p className="newsContent">{content}</p>
-                </div>
-            </div>
+        <div className='mainContainer'>
+            <Header title={news.name} image={news.image} updatedAt={news.updatedAt} />
+            <Content content={news.content} />
+            <GoBackButton />
         </div>
-        
     )
 }
-
-
-
 
 export default NewsDetail
