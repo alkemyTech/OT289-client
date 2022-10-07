390037485374ebdeed853691a3849d80b1e8b854
Facundo Azcue <fazcue@gmail.com>
2022-10-07 19:40:22 -0300

    Change button behavior: delete onClick, add href

--- src/features/screenNews/ScreenNews.js
+++ src/features/screenNews/ScreenNews.js
@@ -17,15 +17,12 @@ const ScreenNews = () => {
             const properties = {
                 method: 'get'
             }
+
             const res = await customFetch(url, properties)
             setNews(res.data)
         } catch (error) {
             console.log('Error getting news: ', error)
         }
-    }
-
-    const redirectToDetailsPage = (id) => {
-        return window.location.href = '/novedades/' + id
     }
 
     const NewsCard = ({ item, index }) => {
@@ -40,7 +37,7 @@ const ScreenNews = () => {
                 <Card.Img variant="top" src={image} className={styles.image} />
                 <Card.Body>
                     <h2 className={styles.title}>{name}</h2>
-                    <Button onClick={() => redirectToDetailsPage(id)} variant="outline-primary">Ver Detalle</Button>
+                    <Button href={`/novedades/${id}`} variant="outline-primary">Ver Detalle</Button>
                 </Card.Body>
             </div>
         )
