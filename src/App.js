14181071100c9e88ba4ea6a344dea8bcb1adead0
Facundo Azcue <fazcue@gmail.com>
2022-10-12 11:50:21 -0300

    Add google fonts + global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 import ScreenContact from './features/screencontac/ScreenContac';
 import Activities from './features/activities/Activities';
