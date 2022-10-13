b3480b1ce0116c076afaf5b418a9c0ae853a5a37
Gabriel <ahrensgabriel09@gmail.com>
2022-10-13 19:05:18 -0300

    Change redirect link

--- src/features/backOffice/AdminRouteGuard.js
+++ src/features/backOffice/AdminRouteGuard.js
@@ -13,7 +13,7 @@ const AdminRouteGuard = ({ element: Component, ...rest }) => {
     }
 
     return(
-        isAdmin() ? <Outlet/> : <Navigate to="/backOffice"/>
+        isAdmin() ? <Outlet/> : <Navigate to="/"/>
     )
 };
  
