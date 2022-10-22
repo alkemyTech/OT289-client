b1dc21bd1c86a55994da0219c22f45df592c9f04
Facundo Azcue <fazcue@gmail.com>
2022-10-22 17:39:49 -0300

    Add customClass prop

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import styles from './CustomButton.module.css'
 
-const CustomButton = ({ href, text }) => {
+const CustomButton = ({ href, text, customClass }) => {
     return (
-        <Button href={href} variant="outline-primary" className={styles.button}>
+        <Button href={href} variant="outline-primary" className={customClass ? customClass : styles.button}>
             {text}
         </Button>
     )
